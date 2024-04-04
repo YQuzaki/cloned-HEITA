ec0082f0b939f0398343f276038c077dabc4c675
YQuzaki <165903073+YQuzaki@users.noreply.github.com>
2024-04-04 23:29:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -139,16 +139,18 @@ if prompt:
     # 构造音频文件的URL（确保这个URL返回音频文件）
     audio_file = f"https://9b98eba910d46bae6c.gradio.live//?spk=HEITA&text={response}&lang=zh"
 
-    # 创建一个不包含controls属性的HTML音频标签
-    audio_html = f"""    
-        <audio autoplay>    
-            <source src="{audio_file}" type="audio/mpeg">    
-            您的浏览器不支持 audio 元素。    
-        </audio>    
-        """
+    st.info("音频加载较慢，会’自动‘播放...（文字越多生成越慢10-30s不等），卡顿请刷新页面")
+    audio_file = f"https://df6748bf4f962d130c.gradio.live//?spk=HEITA&text={response}&lang=zh"
 
-    # 显示一条信息，告诉用户音频正在加载
-    # 使用st.markdown显示音频，并允许不安全的HTML
+    # 创建包含自动播放音频的HTML字符串
+    audio_html = f"""  
+    <audio controls autoplay>  
+        <source src="{audio_file}" type="audio/mpeg">  
+        您的浏览器不支持 audio 元素。  
+    </audio>  
+    """
+
+    # 使用st.components.v1.html来嵌入HTML内容
     st.markdown(audio_html, unsafe_allow_html=True)
     # 使用st.components.v1.html来嵌入HTML内容
         # 将AI的响应添加到历史记录并显示
